d8603bee562c153f3051e2471e5746fbc610fae8
whiteadder <whiteadder@gmail.com>
2019-12-31 00:11:01 +0000

    Updates

---
+++ precache-manifest.c57a15c2622466b3cc09ed7e4506b3ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c12f8a559adb5eec4855ee7d3d9fa94",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d2c35114a5766943d92b",
+    "url": "/monsters-rolodex/static/css/main.d9b03b9c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2c35114a5766943d92b",
+    "url": "/monsters-rolodex/static/js/main.292e166a.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
